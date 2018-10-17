03724f5afbd4846246bd66bd4cb7c79c96264ca5
Glitch (hello-express) <none>
2018-10-17 22:19:17 +0000

    🌷🎎 Checkpoint ./index.js:876534/46 ./watch.json:876534/1940 ./.glitch-assets:876534/86 ./.gitignore:876534/51

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const http = require('http');
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
