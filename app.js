2740acfbe0844b50253e53e2d6fc1f8b78539c10
Paul Hansa <>
2020-01-28 22:08:33 -0500

    Dev env

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require("express");
 const fs = require("fs");
 const Discord = require("discord.js");
-const http = require("http");
-const { execSync } = require("child_process");
 
 const client = new Discord.Client();
 const config = require("./options/config.json");
 
 const app = express();
 
-require("dotenv").config();
-
-// Pulls latest changes from GitHub remote
-// function gitPull() {
-//   console.log("Fetching latest changes.");
-//   const output = execSync("git pull").toString();
-//   console.log(output);
-// }
-// gitPull();
-// setInterval(() => {
-//   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-//   gitPull();
-// }, 301000);
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 client.commandList = new Map();
 fs.readdirSync("./commands").forEach(file => {
