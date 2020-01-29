6787d6b748aaff3a006a2ac432930ae24ffa599c
Paul Hansa <>
2020-01-28 20:27:22 -0500

    Accidentally overwrote this whoops

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const fs = require("fs");
 const Discord = require("discord.js");
+const http = require("http");
+const { execSync } = require("child_process");
 
 const client = new Discord.Client();
 const config = require("./options/config.json");
 
 const app = express();
 
-require("dotenv").config();
+// require('dotenv').config();
+
+// Pulls latest changes from GitHub remote
+function gitPull() {
+  console.log("Fetching latest changes.");
+  const output = execSync("git pull").toString();
+  console.log(output);
+}
+gitPull();
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  gitPull();
+}, 301000);
 
 client.commandList = new Map();
 fs.readdirSync("./commands").forEach(file => {
