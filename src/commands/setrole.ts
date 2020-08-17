b1cde6cd193ce703f26ccb70e49592e975a35eeb
Paul <5748299+biggestcookie@users.noreply.github.com>
2020-08-17 12:46:29 -0400

    Remove setrole

--- src/commands/setrole.ts
+++
@@ -1,8 +0,0 @@
-import { ArgsMap } from "@/utils/command";
-
-export async function execute(args: ArgsMap): Promise<string> {
-  const role = args.get(0) ?? args.get("rolename");
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(role), 5000);
-  });
-}
