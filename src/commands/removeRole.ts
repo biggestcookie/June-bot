aed440481bad0fb52a5ef1eea9edec5bebb47337
Paul <5748299+biggestcookie@users.noreply.github.com>
2020-09-23 15:01:46 +0000

    WIP role delete

--- src/commands/removeRole.ts
+++ src/commands/removeRole.ts
@@ -2,14 +2,14 @@ import { RoleEntity } from "@/entities/role";
 import { ArgsMap, Reply } from "@/utils/command";
 import { Message, MessageEmbed } from "discord.js";
 import { getRepository } from "typeorm";
+import { GuildEntity } from "@/entities/guild";
 
 export async function execute(args: ArgsMap, message: Message): Promise<Reply> {
-  const roleEntities = await getRepository(RoleEntity).find({
-    where: {
-      guild: message.guild.id,
-    },
+  const roleName = args.get(0) ?? args.get("rolename");
+  const roleEntities = await getRepository(RoleEntity).delete({
+    roleName,
+    guild: new GuildEntity(message.guild.id),
   });
-  const roleNames = roleEntities.map((role) => role.roleName);
   const reply = new MessageEmbed();
   return reply;
 }
